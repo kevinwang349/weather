de9ed64f08b449c478713fc3c3700ad532a1e12e
kevinwang349 <kevinwang349.ca@gmail.com>
2023-12-17 21:08:17 -0500

    upload files

---
+++ details/script.js
@@ -0,0 +1,167 @@
+document.addEventListener("DOMContentLoaded",init);
+let map;
+let weatherDays=[];
+let msg=['Actions speak louder than words!','Never put off what you can do today until tomorrow.','Believe in yourself!',"Winners do what losers don't want to do.",'Whatever is worth doing, is worth doing well.','Live well, love lots, and laugh often!','Quitters never win, and winners never quit.','Knowledge is power.','Asking costs nothing.','Better late than never!','Never too old to learn.','Give more than you planned to.'];
+
+async function init() {
+
+    // Create animation div
+    const container=document.getElementById('fadeIn');
+    const mainMsg=document.getElementById('msg');
+    mainMsg.style.fontFamily='fantasy';
+    mainMsg.style.fontSize='40px';
+    const index=Math.floor(Math.random()*(msg.length));
+    mainMsg.innerHTML=msg[index];
+
+    // Loading
+    const loading=document.createElement('p');
+    loading.innerHTML='Waiting for response...';
+    loading.style='position: absolute; z-index: 3';
+    document.body.appendChild(loading);
+
+    // Get coordinates from URL flags
+    const params=new URLSearchParams(window.location.search);
+    const currentPt=[params.get('lat'),params.get('lon')];
+
+    // Get API requests
+    const forecastUrl=`https://api.weatherapi.com/v1/forecast.json?key=2135c58562bd4098a6420220232006&q=${currentPt.join(',')}&days=3`;
+    const forecastTXT=await request(forecastUrl);
+    const forecastJSON=JSON.parse(forecastTXT);
+    weatherDays=forecastJSON.forecast.forecastday;
+    for(let i=1;i<=2;i++){
+        const date=new Date(forecastJSON.location.localtime);
+        date.setDate(date.getDate()-i);
+        const dateStr=`${date.getFullYear()}-${(date.getMonth()+1)<10?'0'+(date.getMonth()+1):(date.getMonth()+1)}-${(date.getDate())<10?('0'+date.getDate()):(date.getDate())}`;
+        const historyUrl=`https://api.weatherapi.com/v1/history.json?key=2135c58562bd4098a6420220232006&q=${currentPt.join(',')}&dt=${dateStr}`;
+        const historyTXT=await request(historyUrl);
+        const historyJSON=JSON.parse(historyTXT);
+        weatherDays.unshift(historyJSON.forecast.forecastday[0]);
+    }
+    document.body.removeChild(loading);
+
+    // Add header
+    const h1=document.createElement('h1');
+    h1.innerHTML=`Weather at ${forecastJSON.location.name}, ${forecastJSON.location.region}, ${forecastJSON.location.country}`;
+    container.appendChild(h1);
+    const localtime=document.createElement('p');
+    const localDate=new Date(forecastJSON.location.localtime);
+    const tmrDate=new Date(localDate.getTime()+86400000);
+    const yesterDate=new Date(localDate.getTime()-86400000);
+    localtime.innerHTML=`Current local time: ${localDate.toLocaleString()}`;
+    container.appendChild(localtime);
+
+    // Display weather in a table
+    const table=document.createElement('table');
+    table.setAttribute('id','weatherTable');
+    const headRow=document.createElement('tr');
+    headRow.innerHTML='<td>Date + Time</td> <td colspan=2>Weather</td> <td>High °C</td> <td>Low °C</td> <td></td>';
+    /*const dateH=document.createElement('td');
+    dateH.innerHTML='Date';
+    headRow.appendChild(dateH);
+    const condH=document.createElement('td');
+    condH.innerHTML='Weather';
+    headRow.appendChild(condH);
+    const hiH=document.createElement('td');
+    hiH.innerHTML='High °C';
+    headRow.appendChild(hiH);
+    const loH=document.createElement('td');
+    loH.innerHTML='Low °C';
+    headRow.appendChild(loH);*/
+    table.appendChild(headRow);
+    for(const day of weatherDays){
+        // Add daily weather
+        const dateObj=new Date(day.date_epoch*1000+localDate.getTimezoneOffset()*60000);
+        const dayRow=document.createElement('tr');
+        //dayRow.setAttribute('id',dateObj.toDateString().substring(0,10));
+        dayRow.id=dateObj.toDateString().substring(0,10);
+        let dateStr=dateObj.toDateString().substring(0,10);
+        if(dateObj.toDateString()==localDate.toDateString()){
+            dayRow.setAttribute('style','font-size: 20px; font-weight: bold;');
+            dateStr+=' (Today)';
+        }else if(dateObj.toDateString()==tmrDate.toDateString()){
+            dateStr+=' (Tomorrow)';
+        }else if(dateObj.toDateString()==yesterDate.toDateString()){
+            dateStr+=' (Yesterday)';
+        }
+        dayRow.innerHTML=`<td>${dateStr}</td>
+        <td><img src="https:${day.day.condition.icon}"></img></td>
+        <td>${day.day.condition.text}</td>
+        <td>${day.day.maxtemp_c}</td>
+        <td>${day.day.mintemp_c}</td>
+        <td><button value='${dateObj.toDateString().substring(0,10)}' onclick='showHourly()'>Show hourly forecast</button></td>
+        `;
+        table.appendChild(dayRow);
+        // Add hourly weather
+        for(const hour of day.hour){
+            const hrRow=document.createElement('tr');
+            hrRow.style.display='none';
+            hrRow.setAttribute('class',dateObj.toDateString().substring(0,10));
+            hrRow.innerHTML=`<td>${hour.time.substring(11)}</td>
+            <td><img src="https:${hour.condition.icon}" width="32px" height="32px"></img></td>
+            <td>${hour.condition.text}</td>
+            <td colspan=2 style='text-align: center;'>${hour.temp_c}</td>
+            `;
+            table.appendChild(hrRow);
+        }
+    }
+    const outerBox=document.createElement('div');
+    outerBox.setAttribute('style','width: 90%; height: 570px; margin: auto; overflow: scroll; border: 2px black solid');
+    outerBox.setAttribute('id','outerBox');
+    outerBox.appendChild(table);
+    container.appendChild(outerBox);
+}
+
+async function showHourly(){
+    const rows=document.querySelectorAll('tr');
+    for(const row of rows){
+        if(row.className==event.target.value){
+            if(row.style.display=='none'){
+                event.target.innerHTML='Hide hourly forecast';
+                row.style.display='';
+            }else{
+                event.target.innerHTML='Show hourly forecast';
+                row.style.display='none';
+            }
+        }
+    }
+}
+
+async function request(url) {
+    let str='';
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'text';
+    request.onload = function () {
+        str+=request.response;
+    };
+    request.send();
+    return new Promise(resolve => {
+        let interval=setInterval(() => {
+            if(str.length>0){
+                clearInterval(interval);
+                resolve(str);
+            }
+        }, 500);
+    });
+}
+
+function draw(){
+    const cvs = document.createElement('canvas');
+    cvs.setAttribute('style', 'height: 20px, width: 20px');
+    const ctx = cvs.getContext('2d');
+    ctx.fillStyle = '#ff0000';
+    ctx.arc(10, 10, 9, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.fillStyle = '#000000';
+    ctx.arc(10, 10, 9, 0, Math.PI * 2);
+    ctx.stroke();
+    const srcUrl = cvs.toDataURL();
+    const circle = L.icon({ iconUrl: srcUrl, iconSize: [200, 100], iconAnchor: [10, 10], popupAnchor: [0, -9] });
+    return circle;
+}
+
+function display(output='') {
+    const p = document.createElement('p');
+    p.innerText = output;
+    document.body.appendChild(p);
+}
---
+++ script.js
@@ -0,0 +1,126 @@
+document.addEventListener("DOMContentLoaded",init);
+let map;
+
+async function init() {
+
+    // Instructions
+    const i1=document.createElement('h1');
+    i1.innerHTML='Double click a location on the map to see the local weather';
+    i1.style.textAlign='center';
+    document.body.appendChild(i1);
+    const i2=document.createElement('h2');
+    i2.innerHTML='Drag to move the map and scroll on the map to zoom in/out';
+    i2.style.textAlign='center';
+    document.body.appendChild(i2);
+    
+    // Create the container div
+    const container = document.createElement('div');
+    container.setAttribute('style', 'width: 90%; margin: auto;');
+    document.body.appendChild(container);
+    
+    // Add weatherAPI link
+    const br=document.createElement('br');
+    document.body.appendChild(br);
+    const a=document.createElement('a');
+    a.innerHTML='<img src="//cdn.weatherapi.com/v4/images/weatherapi_logo.png" alt="Weather data by WeatherAPI.com" border="0">';
+    a.href='https://www.weatherapi.com/';
+    a.style='bottom: 0px; left: 0px; padding: 0px;';
+    a.title='Free Weather API';
+    document.body.appendChild(a);
+
+    // Add about page link
+    const about=document.createElement('a');
+    about.href='https://weather.kevinwang21.repl.co/about';
+    about.innerHTML='About';
+    about.style='position: absolute; bottom: 0px; right: 0px; padding: 10px;';
+    document.body.appendChild(about);
+
+    // Create the div that will hold the Leaflet map
+    const mapdiv = document.createElement('div');
+    mapdiv.setAttribute('style', 'height: 550px;');
+    container.appendChild(mapdiv);
+
+    // Create the map and fill it with tiles
+    map = L.map(mapdiv);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoia2V2aW53MjQwMSIsImEiOiJja3I1ODZqdWszMmdqMnBwYW9qbWVnY2c4In0.qqgVHQu94DuWbLbgjWMN9w'
+    }).addTo(map);
+    //let layerGroup=L.layerGroup();
+
+    // Zoom the map to the world
+    map.setView([0,0],2);
+
+    // Add event listener
+    map.on('dblclick', async function(ev){
+        //layerGroup.clearLayers();
+        const latlng = map.mouseEventToLatLng(ev.originalEvent);
+        let currentPt=[latlng.lat,latlng.lng];
+        if(currentPt[1]>180) currentPt[1]-=360;
+        if(currentPt[1]<-180) currentPt[1]+=360;
+        const currentUrl=`https://api.weatherapi.com/v1/current.json?key=2135c58562bd4098a6420220232006&q=${currentPt.join(',')}`;
+        const response=await request(currentUrl);
+        const json=JSON.parse(response);
+        if(json.error!=undefined&&json.error.message!=undefined){
+            const pop='Sorry, no weather information available in uninhabited areas.';
+            map.openPopup(pop, currentPt);
+            return;
+        }
+        //display(response);
+        currentPt=[json.location.lat,json.location.lon];
+        const date=new Date(json.location.localtime);
+        const pop=`Current weather in ${json.location.name}, ${json.location.region}, ${json.location.country}<br>
+        Temperature ${json.current.temp_c}°C<br>
+        <img src='${json.current.condition.icon}'></img><br>
+        ${json.current.condition.text}<br>
+        Current time: ${date.toLocaleString()}<br>
+        <a href='https://weather.kevinwang21.repl.co/details/?lat=${json.location.lat}&lon=${json.location.lon}'>Click here for weather predictions for this location</a>`;
+        //layerGroup.addLayer(L.marker(currentPt,{icon:draw()}).bindPopup(pop));
+        //layerGroup.addTo(map);
+        map.openPopup(pop, currentPt);
+    });
+}
+
+async function request(url) {
+    let str='';
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'text';
+    request.onload = function () {
+        str+=request.response;
+    };
+    request.send();
+    return new Promise(resolve => {
+        let interval=setInterval(() => {
+            if(str.length>0){
+                clearInterval(interval);
+                resolve(str);
+            }
+        }, 500);
+    });
+}
+
+function draw(){
+    const cvs = document.createElement('canvas');
+    cvs.setAttribute('style', 'height: 20px, width: 20px');
+    const ctx = cvs.getContext('2d');
+    ctx.fillStyle = '#ff0000';
+    ctx.arc(10, 10, 9, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.fillStyle = '#000000';
+    ctx.arc(10, 10, 9, 0, Math.PI * 2);
+    ctx.stroke();
+    const srcUrl = cvs.toDataURL();
+    const circle = L.icon({ iconUrl: srcUrl, iconSize: [200, 100], iconAnchor: [10, 10], popupAnchor: [0, -9] });
+    return circle;
+}
+
+function display(output='') {
+    const p = document.createElement('p');
+    p.innerText = output;
+    document.body.appendChild(p);
+}
